527dc7581f1eaa79b7426573343d3c71ae794cf5
Younes Soleimani <skybird.bits@gmail.com>
2022-11-24 01:38:58 +0330

    links of article could read from here

--- resources/js/main.js
+++ resources/js/main.js
@@ -17,13 +17,35 @@ $(function(){
     loadFooter();
 });
 
+const javaLinks = new Map([
+    ["/article/java/selectors.html", "دستورات شرطی در جاوا"],
+    ["/article/java/switch-statement-jdk-17.html", "دستور switch در jdk-17"],
+    ["/article/java/loops.html", "حلقه ها در جاوا"],
+    ["/article/java/static-keyword.html", "کلیدواژه ی استاتیک در جاوا"]
+]);
 
-const java = {id: 'java_article', isExpanded: false};
-const kotlin = {id: "kotlin_article", isExpanded: false};
-const android = {id: "android_article", isExpanded: false};
-const web = {id: "web_article", isExpanded: false};
+const kotlinLinks = new Map([
+    ["/article/kotlin/extension-function.html", "تابع اکستنشن در کاتلین"],
+    ["/article/kotlin/anonymous-function-and-lambda.html", "عبارت لامبدا و توابع بی نام در کاتلین"]
+]);
 
-const courses = [ java, kotlin, android, web];
+const androidLinks =new Map([
+    ["/article/android/data-binding.html","دیتا بایندینگ در اندروید"],
+    ["/article/android/navigation.html", "نویگیشن در اندروید"],
+    ["/article/android/data-binding-navigation-sample.html","پروژه ی عملی از دیتابایندینگ و نویگیشن در اندروید"],
+    ["/article/android/activity-lifecycle.html", "چرخه ی حیات اکتیویتی در اندروید"]
+]);
+
+const webLinks = new Map([
+    ["/article/web/streaming-spring-boot.html", "ویدیو استریمینگ در اسپرینگ بوت"]
+]);
+
+const java = {id: 'java_article', links: javaLinks, isExpanded: false};
+const kotlin = {id: "kotlin_article",links: kotlinLinks, isExpanded: false};
+const android = {id: "android_article",links: androidLinks, isExpanded: false};
+const web = {id: "web_article", links: webLinks, isExpanded: false};
+
+const courses = [java, kotlin, android, web];
 
 $(function(){
    for(var i =0; i<courses.length; i++){
@@ -31,8 +53,18 @@ $(function(){
 
         var parent = document.getElementById(course.id);
 
-        var linkContainer = parent.querySelector('div');
+
         var btExpand = parent.querySelector('button');
+
+        var linkContainer = parent.querySelector('div');
+
+        course.links.forEach(function(value, key){
+            var anchor = document.createElement('a');
+            anchor.href = key;
+            anchor.innerHTML = value;
+            anchor.classList.add("list-item");
+            linkContainer.appendChild(anchor);
+        });
 
         if(btExpand != null){
             var icon = btExpand.querySelector('span');
