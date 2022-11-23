d011a14570d33bfd29256fc648544ee4207fdf78
Younes Soleimani <skybird.bits@gmail.com>
2022-11-23 23:36:10 +0330

    page index updated, css files are classfied and js main updated

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,23 +2,71 @@ function loadFooter(){
     $('#footer_container').load('/ui-components/shared_footer.html')
 }
 
-function loadStartPageItems(){
-    $('#android_items_container').load('/ui-components/android_tab_items.html');
-    $('#kotlin_items_container').load('/ui-components/kotlin_tab_items.html');
-    $('#java_items_container').load('/ui-components/java_tab_items.html')
-    $('#web_items_container').load('/ui-components/web_tab_items.html')
-}
-
-
 $(function(){
-    var startPage = document.getElementById('start_page');
+    var sidebar = document.getElementById('sidebar');
 
-    if(startPage != null){
-        loadStartPageItems()
+    if(sidebar != null){
+        sidebar.addEventListener('show.bs.offcanvas', function(){
+           $("#brand").hide('slow');
+        });
+        sidebar.addEventListener('hide.bs.offcanvas', function(){
+            $("#brand").show('slow');
+        });
     }
 
     loadFooter();
 });
 
 
+const java = {id: 'java_article', isExpanded: false};
+const kotlin = {id: "kotlin_article", isExpanded: false};
+const android = {id: "android_article", isExpanded: false};
+const web = {id: "web_article", isExpanded: false};
+
+const courses = [ java, kotlin, android, web];
+
+$(function(){
+   for(var i =0; i<courses.length; i++){
+        var course = courses[i];
+
+        var parent = document.getElementById(course.id);
+
+        var linkContainer = parent.querySelector('div');
+        var btExpand = parent.querySelector('button');
+
+        if(btExpand != null){
+            var icon = btExpand.querySelector('span');
+            icon.style.transform = getTransformRotation(90);
+        }
+   }
+});
+
+
+function toggleExpandState(btn){
+    for(var i=0; i<courses.length; i++){
+            var course = courses[i];
+
+            var parent = document.getElementById(course.id);
+            var btExpand = parent.querySelector('button');
+
+            if(btExpand.id == btn.id){
+                var icon = btExpand.querySelector('span');
+                rotate(icon , course);
+                course.isExpanded = !course.isExpanded;
+                break;
+            }
+    }
+}
+
+function rotate(icon , course){
+    if(!course.isExpanded){
+        icon.style.transform = getTransformRotation(0);
+    }else{
+        icon.style.transform = getTransformRotation(90);
+    }
+}
+
+function getTransformRotation(degree){
+    return 'rotate('+ degree +'deg)';
+}
 
