11ffbd619ccd8e570911b276c5f11c8b497a8d8a
Younes Soleimani <skybird.bits@gmail.com>
2022-12-01 05:40:02 +0330

    basic functionalities

--- resources/js/base.js
+++ resources/js/base.js
@@ -5,62 +5,53 @@ const linkCoursesRepo = [
    "/skybirdbits.github.io/resources/json/web-links.json"
 ]
 
-const articleListViewContainer = document.getElementById('article_container');
-
-function loadFooter(){
-    $('#footer_container').load('/ui-components/shared_footer.html');
-}
-
-$(function(){
+function initSidebar(){
     var sidebar = document.getElementById('sidebar');
 
-    if(sidebar != null){
-        sidebar.addEventListener('show.bs.offcanvas', function(){
-           $("#brand").hide('slow');
-        });
-        sidebar.addEventListener('hide.bs.offcanvas', function(){
-            $("#brand").show('slow');
-        });
-    }
-
-    loadFooter();
-});
-
-function rotate(icon , articleObject){
-    if(!articleObject.isExpanded){
-        icon.style.transform = getTransformRotation(0);
-    }else{
-        icon.style.transform = getTransformRotation(90);
-    }
-
-    articleObject.isExpanded = !articleObject.isExpanded;
-}
-
-function getTransformRotation(degree){
-    return 'rotate('+ degree +'deg)';
+        if(sidebar != null){
+            sidebar.addEventListener('show.bs.offcanvas', function(){
+               $("#brand").hide('slow');
+            });
+            sidebar.addEventListener('hide.bs.offcanvas', function(){
+                $("#brand").show('slow');
+            });
+        }
 }
 
 
-function retrieveLinksAndInsertIntoContainer(){
-       if(articleListViewContainer)
-            retrieveLinks();
-}
+/*
+    Functions to create list with links to article subjects
+    views: createArticleListView(article), createListItemView(link) ,createExpandableButton(article),
+    utils: rotate(icon, article), getTransformRotation(degree)
+    json: readJsonFile(url, onResponseOkListener)
+*/
+
 
 //retrieve links stored in json files inside path: /resources/json
-function retrieveLinks(){
-    for(var i =0; i<linkCoursesRepo.length; i++){
-        readJsonFile(linkCoursesRepo[i],function(data){
-            var article = JSON.parse(data);
-            //var articleObject = {id: article.id, title: article.title, links: article.links, isExpanded: false};
+function loadAllArticleLinks(){
+    const articleListViewContainer = document.getElementById('article_container');
 
-            var articleListView = createArticleListView(article);
+    if(articleListViewContainer){
+        for(var i =0; i<linkCoursesRepo.length; i++){
+            readJsonFile(linkCoursesRepo[i],function(data){
 
-            articleListViewContainer.appendChild(articleListView);
+                var articleData = JSON.parse(data);
+                var article = {
+                                id: articleData.id,
+                                title: articleData.title,
+                                links: articleData.links, isExpanded: false
+                               };
 
-        });
+                var articleListView = createArticleListView(article);
+
+                articleListViewContainer.appendChild(articleListView);
+
+            });
+        }
     }
 }
 
+//read a json file with a specific url
 function readJsonFile(url, onResponseOkListener){
     var request = new XMLHttpRequest();
     request.overrideMimeType = "application/json";
@@ -84,7 +75,7 @@ function createArticleListView(article){
     subjectListContainer.classList.add('collapse');
     subjectListContainer.id = article.id;
 
-    var unorderedList = document.createElement('ul');
+    var unorderedList = document.createElement('ol');
     unorderedList.classList.add('list');
 
     var links = article.links;
@@ -101,31 +92,40 @@ function createArticleListView(article){
     return parent;
 }
 
-
-function createExpandableButton(articleObject){
+//create an expandable button for every lists which contain links
+function createExpandableButton(article){
     var btExpand = document.createElement('button');
     btExpand.classList.add('md-bt-expandable');
-    btExpand.dataset.bsTarget = ('#' + articleObject.id);
+    btExpand.dataset.bsTarget = ('#' + article.id);
     btExpand.dataset.bsToggle = 'collapse'
 
     var icon = document.createElement('span');
     icon.classList.add('material-icons');
     icon.innerHTML = 'arrow_drop_down'
-    icon.style.transform = getTransformRotation(90);
+    icon.style.transform = 'rotate(90deg)';
 
     var text = document.createElement('span');
-    text.innerHTML = articleObject.title;
+    text.innerText = article.title;
 
     btExpand.appendChild(icon);
     btExpand.appendChild(text);
 
     btExpand.addEventListener('click', function(){
-            rotate(icon, articleObject);
+            rotate(icon, article);
     });
 
     return btExpand;
 }
 
+function rotate(icon , article){
+    if(!article.isExpanded){
+        icon.style.transform = 'rotate(0deg)';
+    }else{
+        icon.style.transform = 'rotate(90deg)';
+    }
+
+    article.isExpanded = !article.isExpanded;
+}
 
 function createListItemView(link){
    var item = document.createElement('li');
@@ -139,3 +139,52 @@ function createListItemView(link){
 
    return item;
 }
+
+
+//create and load items of footer
+function createAndLoadFooterItems(){
+    let footer = document.querySelector('footer');
+
+    let brandLicenseContainer = createFooterItemContainer('brand-license-container');
+
+    let itemBrandLicense = createFooterItem("2022 Skybirdbits, GPL","https://github.com/skybirdbits/" )
+    brandLicenseContainer.appendChild(itemBrandLicense);
+
+    let telegramLinksContainer = createFooterItemContainer('telegram-links-container');
+
+    let itemTelegramChannel = createFooterItem("کانال تلگرام", "https://t.me/java_kotlin_dev");
+    let itemTelegramGroup = createFooterItem("گروه تلگرام" ,"https://t.me/java_kotlin_dev_gp");
+
+    telegramLinksContainer.append(itemTelegramChannel, itemTelegramGroup);
+
+    let socialMediaContainer = createFooterItemContainer('social-media-container');
+
+    let itemLinkedIn = createFooterItem("Linkedin","https://linkedin.com/in/skybirdbits")
+    let itemGithub = createFooterItem("Github","https://github.com/skybirdbits")
+
+    socialMediaContainer.append(itemLinkedIn, itemGithub);
+
+    footer.append(brandLicenseContainer, telegramLinksContainer, socialMediaContainer);
+}
+
+function createFooterItemContainer(styleClassName){
+    let container = document.createElement('div');
+    container.classList.add('footer-item-container', styleClassName);
+    return container;
+}
+
+function createFooterItem(text, href){
+    let item = document.createElement('a');
+    item.innerText = text;
+    item.href = href;
+    item.classList.add('footer-item');
+    return item;
+}
+
+function base(){
+    createAndLoadFooterItems();
+    initSidebar();
+    loadAllArticleLinkLists();
+}
+
+export {initSidebar , loadAllArticleLinkLists , createAndLoadFooterItems};
