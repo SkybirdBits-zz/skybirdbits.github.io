f1357e5fb54df42d96e915e00b6545099e75af3c
Younes Soleimani <skybird.bits@gmail.com>
2022-12-01 07:13:56 +0330

    moved js and css loader functions

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,22 +1,20 @@
 import {createCodeViews} from './article.js';
 import {initSidebar , loadAllArticleLinks , createAndLoadFooterItems} from './base.js';
+import {loadStylesheet, loadJavaScript} as fileloader from './fileloader.js';
 
+fileloader.loadStylesheet(
+"https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" ,
+"sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi",
+"anonymous"
+);
 
-function loadJsFile(url ,integrity,crossOrigin){
-    const script = document.createElement('script');
-    script.src = url;
-    script.integrity = integrity;
-    script.crossOrigin = crossOrigin;
-    document.head.appendChild(script);
-}
-
-loadJsFile(
+fileloader.loadJavaScript(
 "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js",
 "sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3",
 "anonymous"
 );
 
-loadJsFile(
+fileloader.loadJavaScript(
 "https://code.jquery.com/jquery-3.6.1.min.js",
 "sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=",
 "anonymous"
